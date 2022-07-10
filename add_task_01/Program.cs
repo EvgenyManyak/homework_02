245309b03db743bfcb38f60dd01346b9bf73d348
Evgeny <buriy-anarchist@inbox.ru>
2022-07-10 20:36:40 +0300

    Add task 01

---
+++ add_task_01/Program.cs
@@ -0,0 +1,6 @@
+﻿int h, a, b, s;
+h = Convert.ToInt32(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+s = ((h - a) / (a - b) + 1);
+Console.WriteLine(s);
