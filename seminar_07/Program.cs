1d9ff7af9a6084473c0c57129056313448a4dd3d
Evgeny <buriy-anarchist@inbox.ru>
2022-07-14 10:28:58 +0300

    Add sem 07

---
+++ seminar_07/Program.cs
@@ -0,0 +1,28 @@
+﻿int[, ] CreateMatr(int n, int m)
+{
+    int[, ] matrix = new int[n ,m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatr(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatr(CreateMatr(3, 4));
+
